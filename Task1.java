c0793b1c88e17852f999e89acf5bbb8df28f383b
MADA-01 <markanthonyagcaoili13@gmail.com>
2026-03-25 23:19:57 +0800

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+    /** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+*/
+    public static void main(String[] nihao) {
+        
+    String first = "H3ll0"; // this is a just test sir instead hello i change it to h3llo, even i change the charater still the output will provide
+    String last = "World";
+    String full =first+last;
+        System.out.println("Hello World!");
+    }
+}
+
