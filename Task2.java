6c03277d2511e95b4c571e2c2a7a338af7537b4d
MADA-01 <markanthonyagcaoili13@gmail.com>
2026-03-25 23:20:55 +0800

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,26 @@
+public class Task2 {
+    
+    public static void main(String[] betatest) {
+
+        // we need to declare each primitive is to set a value to the contributes to final string
+         byte b = 0; 
+         short s = 2;
+         int i = 3;
+         float f = 2.0f;
+         char c1 = 'H'; 
+         char c2 = 'I';           
+         char c3 = 'I';    
+         char c4 = 'W';    
+         char c5 = 'R';    
+         char c6 = 'D';    
+         Boolean bool = true;
+
+         String output = c1 +
+         String.valueOf(i) +c2 +c3 + b + " " +c4 + b + c5 + c2 + c6 + "" + f + "" + bool;
+         System.out.println(output); // to show this we need to say output
+         
+    }
+
+}
+
+
